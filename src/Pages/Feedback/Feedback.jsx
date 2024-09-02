12345af8b0bee4aee7647fca33670fc79dc20d9f
Jose Gonzalez <jdariogonzalez.2020@gmail.com>
2024-09-02 18:02:15 -0300

    add feature feedback

--- src/Pages/Feedback/Feedback.jsx
+++ src/Pages/Feedback/Feedback.jsx
@@ -1,23 +1,45 @@
 import { useState } from "react";
-import { Form } from "./Feedback.styles";
+import { useMessagesContext } from "../../contexts/MessagesContext";
+import {
+  Container,
+  FeedbackTitle,
+  Form,
+  FormContainer,
+  Textarea,
+  TitleContainer,
+} from "./Feedback.styles";
+import { useLanguage } from "../../contexts/LanguageContext";
 
 const Feedback = () => {
   const [isFormOpen, setIsFormOpen] = useState(false);
+
+  const { texts } = useLanguage();
 
   const toggleFomr = () => {
     setIsFormOpen(!isFormOpen);
   };
 
   return (
-    <div>
-      <h1>Déjame tu comentário</h1>
-      <button onClick={toggleFomr}>Clic para comentar</button>
-      <Form $isOpen={isFormOpen}>
-        <textarea placeholder="Comentar"></textarea>
-        <button>Comentar</button>
-      </Form>
-      <p>This is a feedback page</p>
-    </div>
+    <Container>
+      <TitleContainer>
+        <FeedbackTitle>
+          <span>{texts.feedback.title}</span>
+          <span>{texts.feedback.description}</span>
+        </FeedbackTitle>
+        <button onClick={toggleFomr}>
+          {isFormOpen ? "Clic para ocultar formulário" : "Clic para comentar"}
+        </button>
+      </TitleContainer>
+      <FormContainer>
+        <Form $isOpen={isFormOpen}>
+          <Textarea placeholder="Comentar"></Textarea>
+          <button>Comentar</button>
+        </Form>
+      </FormContainer>
+      <div>
+        <p>This is a feedback page</p>
+      </div>
+    </Container>
   );
 };
 
--- src/Pages/Feedback/Feedback.styles.js
+++ src/Pages/Feedback/Feedback.styles.js
@@ -1,5 +1,106 @@
 import styled from "styled-components";
+import responsive from "../../Styles/responsive";
 
 export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  margin: 1rem 0;
+
   display: ${({ $isOpen }) => ($isOpen ? "flex" : "none")};
+`;
+
+export const Container = styled.div`
+  max-width: 90%;
+  margin: 0 auto;
+  padding: 1.25rem;
+  background-color: ${({ theme }) => theme.background};
+  color: ${({ theme }) => theme.text};
+  border-radius: 8px;
+
+  ${responsive.lg`
+  max-width: 85%;
+	margin: 1rem
+	padding: 2.25rem;
+  `}
+
+  ${responsive.xl`
+	margin: 3rem
+	padding: 3.25rem;
+    `}
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const FeedbackTitle = styled.h1`
+  font-size: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  text-align: center;
+  padding: 1rem;
+
+  span {
+    background-color: ${({ theme }) => theme.menuBackground};
+    color: ${({ theme }) => theme.accent};
+    padding: 0 10px;
+    border-radius: 5px;
+  }
+
+  ${responsive.lg`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    font-size: 2rem;
+	  margin: 1rem
+	  padding: 2.25rem;
+  `}
+
+  ${responsive.xl`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    font-size: 2.5rem;
+    margin: 3rem
+    padding: 3.25rem;
+    `}
+`;
+
+export const FormContainer = styled.div`
+  margin: 0 auto;
+`;
+
+export const Textarea = styled.textarea`
+  padding: 10px;
+  font-size: 16px;
+  border: 2px solid ${({ theme }) => theme.border};
+  border-radius: 4px;
+  background-color: ${({ theme }) => theme.inputBackground};
+  color: ${({ theme }) => theme.background};
+  resize: none;
+  min-height: 150px;
+  width: 90vw;
+
+  &:focus {
+    outline: none;
+    border-color: ${({ theme }) => theme.accent};
+  }
+
+  ${responsive.lg`
+  max-width: 60%;
+	margin: 1rem
+	padding: 2.25rem;
+  `}
+
+  ${responsive.xl`
+  max-width: 50%;
+	margin: 3rem
+	padding: 3.25rem;
+    `}
 `;

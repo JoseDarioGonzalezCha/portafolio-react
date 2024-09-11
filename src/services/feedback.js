558858d324219f6be42edf2d98467ea5c3f30124
Jose Gonzalez <jdariogonzalez.2020@gmail.com>
2024-09-11 16:25:32 -0300

    update urls

--- src/services/feedback.js
+++ src/services/feedback.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://json-server-api-09kw.onrender.com",
+  baseURL: "https://json-server-api-09kw.onrender.com1",
 });
 
 export const sendFeedback = async (feedbackData) => {
